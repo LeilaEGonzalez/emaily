90e20a15a413c14d6cafbcf0574e1b7a4b9584e3
Leila Elizabeth Gonzalez <layla.elizabeth95@gmail.com>
2022-05-16 22:49:33 -0300

    added api and netlify connection

--- src/api.js
+++ src/api.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("../config/keys");
 
 const app = express();
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.json({ hi: "there" });
-});
-app.use("/.netlify/functions/api", router);
 
+app.use("/.netlify/functions/api", router);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "http://localhost:9000/.netlify/functions/api/",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+app.get(
+  "/.netlify/functions/api",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 module.exports.handler = serverless(app);
