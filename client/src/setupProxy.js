7cfd8e9abdcceb076767f43cb66b3b21e8e19989
Leila Elizabeth Gonzalez <layla.elizabeth95@gmail.com>
2022-05-19 20:43:18 -0300

    react adition and proxies

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+	app.use(
+		['/.netlify/functions/api/'],
+		createProxyMiddleware({
+			target: 'http://localhost:9000/.netlify/functions/api/',
+		})
+	);
+};
