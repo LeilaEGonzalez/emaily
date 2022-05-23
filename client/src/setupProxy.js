57a16cc24e430630dd134d92dfb787ad244e05b1
Leila Elizabeth Gonzalez <layla.elizabeth95@gmail.com>
2022-05-23 16:58:34 -0300

    fetchUser adition and more

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
 	app.use(
-		['/.netlify/functions/api/'],
+		['/.netlify/functions/api/', '/.netlify/functions/api/**'],
 		createProxyMiddleware({
 			target: 'http://localhost:9000/.netlify/functions/api/',
 		})
--- services/passport.js
+++ services/passport.js
@@ -21,22 +21,16 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL:
-				(process.env.NODE_ENV === 'production'
-					? paths.prod
-					: paths.dev) + paths.api,
+			callbackURL: (process.env.NODE_ENV === 'production' ? paths.prod : paths.dev) + paths.api,
 			proxy: true,
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id })
-						.save()
-						.then((user) => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
 		}
 	)
 );
